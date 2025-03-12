af278c79ff284e1c6348b5771760a007591acf8e
Marielaquintanar <marielaqntrr@gmail.com>
2025-03-11 19:29:25 -0600

    agregar mas libros y se muestran todos los datos en el grid

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -5,10 +5,18 @@ const DataGrid = ({ books }) => {
     <div className="container">
       <div className='header'>Libro</div>
       <div className='header'>Autor</div>
+      <div className='header'>Genero</div>
+      <div className='header'>Fecha de Publicacion</div>
+      <div className='header'>ISBN</div>
+      <div className='header'>Disponibilidad</div>
       {books.map((book) => (
         <div key={book.isbn} className="book-row">
           <div>{book.title}</div>
           <div>{book.author}</div>
+          <div>{book.genre}</div>
+          <div>{book.published_date}</div>
+          <div>{book.isbn}</div>
+          <div>{book.availability}</div>
         </div>
       ))}
     </div>
